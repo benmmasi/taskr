e596f51f4c006f2b4ec2721e04ba3bde4b58ec84
Ben Masi <ben.m.masi@gmail.com>
2014-10-21 13:32:46 -0400

    Error messages pop up when title or description of task is not filled out

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,11 +5,17 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = current_user.tasks.create(task_params)
-
-    redirect_to tasks_path
-
+    @task = Task.new(task_params)
+    @tasks = current_user.tasks
+    if @task.save
+      current_user.tasks.create(task_params)
+      redirect_to tasks_path
+    else
+      render :index
+    end
   end
+
+
 
   private
 
